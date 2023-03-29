5d13455976cceb47c3d6b0b30a077b95801c1004
krishnakoumar27 <krishnakoumar27@gmail.com>
2023-03-29 23:16:02 +0530

    Initial Layout

---
+++ data.py
@@ -0,0 +1,31 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores aliquam tempora quasi. Eveniet ipsa voluptatibus sapiente culpa officiis laboriosam incidunt ex iste voluptatem sunt, dicta tenetur dolores, amet, voluptates ullam?',
+            'author': 'krishna',
+            'create_date': '29-03-2023'
+            
+        },
+
+        {
+            'id':2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores aliquam tempora quasi. Eveniet ipsa voluptatibus sapiente culpa officiis laboriosam incidunt ex iste voluptatem sunt, dicta tenetur dolores, amet, voluptates ullam?',
+            'author': 'krishna',
+            'create_date': '29-03-2023'
+            
+        },
+
+        {
+            'id':3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores aliquam tempora quasi. Eveniet ipsa voluptatibus sapiente culpa officiis laboriosam incidunt ex iste voluptatem sunt, dicta tenetur dolores, amet, voluptates ullam?',
+            'author': 'krishna',
+            'create_date': '29-03-2023'
+            
+        }
+        
+    ]
+    return articles
